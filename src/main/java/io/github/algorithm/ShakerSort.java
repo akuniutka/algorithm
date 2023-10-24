471dfb8ebe510fb2299e68daea9a5612efcf0ef0
Andrei Kuniutka <akuniutka@gmail.com>
2023-10-25 00:40:20 +0300

    improved ShakerSort by  interim checks

--- src/main/java/io/github/algorithm/ShakerSort.java
+++ src/main/java/io/github/algorithm/ShakerSort.java
@@ -4,18 +4,29 @@ public class ShakerSort {
     public void sort(int[] array) {
         int left = 0, right = array.length - 1;
         while (left < right) {
+            boolean wasSwap = false;
             for (int i = left; i < right; ++i) {
                 if (array[i] > array[i + 1]) {
                     swap(array, i, i + 1);
+                    wasSwap = true;
                 }
             }
             --right;
+            if (!wasSwap) {
+                return;
+            } else {
+                wasSwap = false;
+            }
             for (int i = right; i > left; --i) {
                 if (array[i - 1] > array[i]) {
                     swap(array, i - 1, i);
+                    wasSwap = true;
                 }
             }
             ++left;
+            if (!wasSwap) {
+                return;
+            }
         }
     }
 
