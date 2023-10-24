c51a1fc1b95f291a6b7d30f53197858730702acb
Andrei Kuniutka <akuniutka@gmail.com>
2023-10-25 00:36:02 +0300

    improved BubbleSort by  interim checks; added swap() method

--- src/main/java/io/github/algorithm/BubbleSort.java
+++ src/main/java/io/github/algorithm/BubbleSort.java
@@ -3,13 +3,22 @@ package io.github.algorithm;
 public class BubbleSort {
     public void sort(int[] array) {
         for (int iteration = 1; iteration < array.length; ++iteration) {
+            boolean wasSwap = false;
             for (int i = 0; i < array.length - iteration; ++i) {
                 if (array[i + 1] < array[i]) {
-                    int temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
+                    swap(array, i, i + 1);
+                    wasSwap = true;
                 }
             }
+            if (!wasSwap) {
+                return;
+            }
         }
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
