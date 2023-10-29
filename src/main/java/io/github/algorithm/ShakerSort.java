87951e72d5500e87f2b6a80c804b9b6915f0781e
Andrei Kuniutka <akuniutka@gmail.com>
2023-10-29 19:56:18 +0300

    improved ShakerSort by catching the last element moved during an iteration

--- src/main/java/io/github/algorithm/ShakerSort.java
+++ src/main/java/io/github/algorithm/ShakerSort.java
@@ -3,30 +3,25 @@ package io.github.algorithm;
 public class ShakerSort {
     public void sort(int[] array) {
         int left = 0, right = array.length - 1;
+        int lastSwapped = left;
         while (left < right) {
-            boolean wasSwap = false;
             for (int i = left; i < right; ++i) {
                 if (array[i] > array[i + 1]) {
                     swap(array, i, i + 1);
-                    wasSwap = true;
+                    lastSwapped = i + 1;
                 }
             }
-            --right;
-            if (!wasSwap) {
+            right = --lastSwapped;
+            if (left >= right) {
                 return;
-            } else {
-                wasSwap = false;
             }
             for (int i = right; i > left; --i) {
                 if (array[i - 1] > array[i]) {
                     swap(array, i - 1, i);
-                    wasSwap = true;
+                    lastSwapped = i - 1;
                 }
             }
-            ++left;
-            if (!wasSwap) {
-                return;
-            }
+            left = ++lastSwapped;
         }
     }
 
