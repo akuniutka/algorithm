e0b7632e7575bf4a85c89bb3b9aa2b06e47671b4
Andrei Kuniutka <akuniutka@gmail.com>
2023-12-16 13:08:30 +0300

    added an insertion sort implementation

---
+++ src/main/java/io/github/akuniutka/algorithm/sort/InsertionSort.java
@@ -0,0 +1,15 @@
+package io.github.akuniutka.algorithm.sort;
+
+public class InsertionSort {
+    public void sort(int[] array) {
+        for (int i = 1; i < array.length; ++i) {
+            int temp = array[i];
+            int j = i;
+            while (j > 0 && array[j - 1] > temp) {
+                array[j] = array[j - 1];
+                --j;
+            }
+            array[j] = temp;
+        }
+    }
+}
