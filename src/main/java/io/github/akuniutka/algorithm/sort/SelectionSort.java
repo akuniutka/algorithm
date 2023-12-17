23df68478ab335d30a2c9de0f182697581b9451e
Andrei Kuniutka <akuniutka@gmail.com>
2023-12-17 13:30:39 +0300

    added an selection sort implementation

---
+++ src/main/java/io/github/akuniutka/algorithm/sort/SelectionSort.java
@@ -0,0 +1,17 @@
+package io.github.akuniutka.algorithm.sort;
+
+public class SelectionSort {
+    public void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; ++i) {
+            int minAt = i;
+            for (int j = i; j < array.length; ++j) {
+                if (array[j] < array[minAt]) {
+                    minAt = j;
+                }
+            }
+            int temp = array[minAt];
+            array[minAt] = array[i];
+            array[i] = temp;
+        }
+    }
+}
