a76f74df8ee6a3a19085590fac245c3bf83556df
Andrei Kuniutka <akuniutka@gmail.com>
2024-01-03 22:39:39 +0300

    added a three-way-partition quicksort implementation

---
+++ src/test/java/io/github/akuniutka/algorithm/sort/HoarePartitionQuickSortTest.java
@@ -0,0 +1,7 @@
+package io.github.akuniutka.algorithm.sort;
+
+public class HoarePartitionQuickSortTest extends SortTestCases {
+    public HoarePartitionQuickSortTest() {
+        super(HoarePartitionQuickSort::sort);
+    }
+}
