20d1038537f7fb57806d804d850125bc16279fdd
Andrei Kuniutka <akuniutka@gmail.com>
2024-01-07 14:41:04 +0300

    refactored a heapsort implementation

--- src/main/java/io/github/akuniutka/algorithm/sort/HeapSort.java
+++ src/main/java/io/github/akuniutka/algorithm/sort/HeapSort.java
@@ -10,22 +10,26 @@ public class HeapSort extends AbstractSort {
             } else {
                 --start;
             }
-            int element = start;
-            while (true) {
-                int child = element * 2 + 1;
-                if (child > end) {
-                    break;
-                }
-                if (child + 1 <= end && array[child] < array[child + 1]) {
-                    child = child + 1;
-                }
-                if (array[element] < array[child]) {
-                    swap(array, element, child);
-                    element = child;
-                } else {
-                    break;
-                }
-            }
+            siftElement(array, start, end);
         }
+    }
+
+    private static void siftElement(int[] array, int element, int end) {
+        int child = greaterChild(array, element, end);
+        while (child != -1 && array[element] < array[child]) {
+            swap(array, element, child);
+            element = child;
+            child = greaterChild(array, element, end);
+        }
+    }
+
+    private static int greaterChild(int[] array, int parent, int end) {
+        int child = parent * 2 + 1;
+        if (child > end) {
+            return -1;
+        } else if (child + 1 <= end && array[child] < array[child + 1]) {
+            ++child;
+        }
+        return child;
     }
 }
