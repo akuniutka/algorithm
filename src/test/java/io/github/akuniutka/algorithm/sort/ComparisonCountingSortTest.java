471a88a1b6286ab6e2b849979acdda4df6007921
Andrei Kuniutka <akuniutka@gmail.com>
2024-01-09 22:07:40 +0300

    added a comparison counting sort implementation

---
+++ src/test/java/io/github/akuniutka/algorithm/sort/ComparisonCountingSortTest.java
@@ -0,0 +1,7 @@
+package io.github.akuniutka.algorithm.sort;
+
+public class ComparisonCountingSortTest extends SortTestCases {
+    public ComparisonCountingSortTest() {
+        super(ComparisonCountingSort::sort);
+    }
+}
